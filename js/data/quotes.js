bb742c74cd59e1e804e3ead2ee22a0f61f574cd8
gauravaccentureproducts <243459266+gauravaccentureproducts@users.noreply.github.com>
2025-11-26 17:52:42 +0900

    Add files via upload

---
+++ js/data/quotes.js
@@ -0,0 +1,18 @@
+window.motivationalQuotes = [
+    "The only bad workout is the one that didn't happen!",
+    "Discipline is doing what needs to be done!",
+    "You don't have to be great to start, but you have to start to be great!",
+    "Success is what comes after you stop making excuses!",
+    "The pain you feel today will be the strength you feel tomorrow!",
+    "Don't wish for it, work for it!",
+    "Your body can stand almost anything. It's your mind you have to convince!",
+    "The only person you should try to be better than is the person you were yesterday!",
+    "No pain, no gain. Shut up and train!",
+    "The hardest lift is lifting your butt off the couch!",
+    "Excuses don't burn calories!",
+    "Sweat is just fat crying!",
+    "Your workout is my warm-up!",
+    "Train insane or remain the same!",
+    "Sore today, strong tomorrow!",
+    "Don't stop when you're tired. Stop when you're done!"
+];
